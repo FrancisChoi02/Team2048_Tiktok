d8925532be201ed0298c9761ad7960c7b9983702
Francis8349 <francis834903237@gmail.com>
2023-02-13 03:41:10 +0800

    键值对键名调整

--- model/redis_keys.go
+++ model/redis_keys.go
@@ -6,15 +6,15 @@ package model
 const (
 	KeyPrefix = "Tiktok2048" //统一该项目在Redis数据库中的前缀，方便检索
 	//互动接口
-	KeyVideoScoreZset       = "video:score"     //视频 以及 累积的点赞数量
-	KeyVideoLikedZsetPrefix = "video:liked:"    //这是 键 的前缀，搭配 视频ID 成为一个完成的键；值是对该视频按了点赞的用户ID
-	KeyUserFavorZsetPrefix  = "video:favorite:" //这是 键 的前缀，搭配 用户ID 成为一个完整的键；值是该用户点过赞的视频ID
+	KeyVideoScoreZset      = "video:score"     //视频 以及 累积的点赞数量
+	KeyVideoLikedSetPrefix = "video:liked:"    //这是 键 的前缀，搭配 视频ID 成为一个完成的键；值是对该视频按了点赞的用户ID
+	KeyUserFavorSetPrefix  = "video:favorite:" //这是 键 的前缀，搭配 用户ID 成为一个完整的键；值是该用户点过赞的视频ID
 
 	//社交接口
-	KeyUserFansZsetPrefix   = "user:fans:"   //这是 键 的前缀，搭配 用户ID 成为一个完整的键；值是该用户的粉丝的ID
-	KeyUserFollowZsetPrefix = "user:follow:" //这是 键 的前缀，搭配 用户ID 成为一个完整的键；值是该用户关注的博主的ID
+	KeyUserFansSetPrefix   = "user:fans:"   //这是 键 的前缀，搭配 用户ID 成为一个完整的键；值是该用户的粉丝的ID
+	KeyUserFollowSetPrefix = "user:follow:" //这是 键 的前缀，搭配 用户ID 成为一个完整的键；值是该用户关注的博主的ID
 	//好友列表 不一定用redis
-	KeyFriendshipZsetPrefix = "user:friend:" //这是 键 的前缀，搭配 用户ID 成为一个完整的键；值是该用户聊天列表中的好友
+	KeyFriendshipSetPrefix = "user:friend:" //这是 键 的前缀，搭配 用户ID 成为一个完整的键；值是该用户聊天列表中的好友
 
 )
 
